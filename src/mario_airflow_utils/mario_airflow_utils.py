8105e7561ec23b23ab1fc18d726a94791cbb23dc
dMario24 <data.mario24@gmail.com>
2023-06-12 10:40:07 +0900

    0.7 exe_kr

--- src/mario_airflow_utils/mario_airflow_utils.py
+++ src/mario_airflow_utils/mario_airflow_utils.py
@@ -23,6 +23,33 @@ from airflow.operators.bash import BashOperator
 
 nnn = 1
 
+"""2020-11-11 형식의 날짜 반환"""
+exe_kr = '{{ execution_date.add(hours=9).format("%Y-%m-%d") }}'
+
+"""20201212 형식의 날짜 반환"""
+exe_kr_nodash = '{{ execution_date.add(hours=9).format("%Y%m%d") }}'
+
+"""2020-11-11 형식의 날짜 반환 + 한달 더하기"""
+exe_kr_add_months = '{{ execution_date.add(hours=9).add(months=1).format("%Y-%m-%d") }}'
+
+"""2020-11-11 형식의 날짜 반환 + 하루 더하기"""
+exe_kr_add_days = '{{ execution_date.add(hours=9).add(days=1).format("%Y-%m-%d") }}'
+
+"""2020-11-11 형식의 날짜 반환 - 일주일 빼기"""
+exe_kr_a_week_ago = '{{ execution_date.add(hours=9).add(days=-7).format("%Y-%m-%d") }}'
+
+"""2020-11-11 형식의 날짜 반환 - 한달 빼기"""
+exe_kr_a_month_ago = '{{ execution_date.add(hours=9).add(months=-1).format("%Y-%m-%d") }}'
+
+"""2020-11-11 형식의 날짜 반환 - 1년 빼기"""
+exe_kr_1_year_ago = '{{ execution_date.add(hours=9).add(years=-1).format("%Y-%m-%d") }}'
+
+"""2020-11-11 형식의 날짜 반환 - 2년 빼기"""
+exe_kr_2_year_ago = '{{ execution_date.add(hours=9).add(years=-2).format("%Y-%m-%d") }}'
+
+"""2020-11-11 형식의 날짜 반환 - 하루 빼기"""
+exe_kr_yesterday = '{{ execution_date.add(hours=9).add(days=-1).format("%Y-%m-%d") }}'
+
 def hello():
     print("hi")
     return "hello"
